ac326b2259def4c5e2739d55434ea00ae2444ab6
nhansoihehe <nhansoihehe@gmail.com>
2019-10-03 11:38:53 +0700

    File login, database v1

---
+++ DBConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database name here.
+$DatabaseName = "douong";
+
+//Define your database username here.
+$HostUser = "root";
+
+//Define your database password here.
+$HostPass = "";
+
